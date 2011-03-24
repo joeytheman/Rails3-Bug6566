add35993728d5709279477049287f558cadeb4ec
unknown <Joseph@.(none)>
2011-03-24 16:14:53 -0500

    Uncommented validation

--- app/models/users_team.rb
+++ app/models/users_team.rb
@@ -2,6 +2,6 @@ class UsersTeam < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :team
 	
-	#validates_presence_of :position
-	#validates_uniqueness_of :position, :scope => :team_id #Two users with the same position cannot be on the same team
+	validates_presence_of :position
+	validates_uniqueness_of :position, :scope => :team_id #Two users with the same position cannot be on the same team
 end
